59d2a6421931d9b1991ddf5816807d4f8384c186
srikarb25 <bommidenisrikar95@gmail.com>
2024-11-17 20:59:34 +0530

    java

---
+++ src/main/java/segroup/Java_maven_project/App.java
@@ -0,0 +1,13 @@
+package segroup.Java_maven_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
